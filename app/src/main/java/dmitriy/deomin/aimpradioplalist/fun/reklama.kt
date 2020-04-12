a25969f830da7afa4778aa3628d35e43accfd867
Deomin Dmitriy <58627@bk.ru>
2020-04-12 21:09:03 +0300

    reliz 91 no_ads (потом вернуть)

--- app/src/main/java/dmitriy/deomin/aimpradioplalist/fun/reklama.kt
+++ app/src/main/java/dmitriy/deomin/aimpradioplalist/fun/reklama.kt
@@ -1,26 +1,35 @@
 package dmitriy.deomin.aimpradioplalist.`fun`
 
-import android.view.View
-import com.google.android.gms.ads.AdRequest
-import com.google.android.gms.ads.AdView
-import com.google.android.gms.ads.MobileAds
-import dmitriy.deomin.aimpradioplalist.Main
-import dmitriy.deomin.aimpradioplalist.R
+//import android.view.View
+//import com.google.android.gms.ads.AdRequest
+//import com.google.android.gms.ads.AdView
+//import com.google.android.gms.ads.MobileAds
+//import dmitriy.deomin.aimpradioplalist.Main
+//import dmitriy.deomin.aimpradioplalist.R
+//
+//fun reklama(view: View){
+//    //-------------------------------------------------------------------------
+//    MobileAds.initialize(Main.context, "ca-app-pub-7908895047124036~7402987509")
+//    val mAdView: AdView = view.findViewById(R.id.adView)
+//    val adRequest = AdRequest.Builder().build()
+//    mAdView.loadAd(adRequest)
+//    //--------------------------------------------------------------------------
+//    //если нажмут кнопку доната в о проге то отключим показ рекламы
+//    if (save_read("reklama_pokaz") == "of") {
+//        mAdView.visibility = View.GONE
+//    } else {
+//        mAdView.visibility = View.VISIBLE
+//    }
+//    //-----------------------------------------------------------------------
 
-fun reklama(view: View){
-    //-------------------------------------------------------------------------
-    MobileAds.initialize(Main.context, "ca-app-pub-7908895047124036~7402987509")
-    val mAdView: AdView = view.findViewById(R.id.adView)
-    val adRequest = AdRequest.Builder().build()
-    mAdView.loadAd(adRequest)
-    //--------------------------------------------------------------------------
-    //если нажмут кнопку доната в о проге то отключим показ рекламы
-    if (save_read("reklama_pokaz") == "of") {
-        mAdView.visibility = View.GONE
-    } else {
-        mAdView.visibility = View.VISIBLE
-    }
-    //-----------------------------------------------------------------------
+
+
+
+
+
+
+
+
 //        GlobalScope.launch(Dispatchers.IO) {
 //            delay(7000)
 //            try{
@@ -31,4 +40,11 @@ fun reklama(view: View){
 //
 //            }
 //        }
-}
+
+
+
+
+
+
+
+//}
